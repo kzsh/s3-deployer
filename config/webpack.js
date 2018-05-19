e7c25c097d77866d16acccf90fd221562cb69e36
Andrew Hunt <andrew@hunt.li>
2018-05-19 14:35:07 -0400

    Adapt webpack routine to node app

--- config/webpack.js
+++ config/webpack.js
@@ -1,10 +1,7 @@
 const path = require('path');
 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const BASE_DIR = path.resolve(__dirname, '../');
 
@@ -14,73 +11,72 @@ const PATHS = {
 };
 
 const APP_PATHS = [
-  'javascript',
-  'html',
-  'stylesheets'
+  'javascript'
 ].reduce((appPaths, srcPath) => (appPaths[srcPath] = `${PATHS.src}/${srcPath}`) && appPaths, {});
-
-const ExtractCSSPlugin = new ExtractTextPlugin({
-  allChunks: true,
-  filename: 'bundle.[chunkhash].css'
-});
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  target: 'node',
   entry: {
-    main: `${APP_PATHS.javascript}/app.js`
+    core: `${APP_PATHS.javascript}/core/index.js`
   },
   output: {
     path: PATHS.dist,
-    filename: 'bundle.[chunkhash].js'
+    filename: 'deployer.js',
+    library: 'Deployer',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    'yargs': {
+      commonjs: 'yargs',
+      commonjs2: 'yargs'
+    },
+    'chalk': {
+      commonjs: 'chalk',
+      commonjs2: 'chalk'
+    },
+    'fs': {
+      commonjs: 'fs',
+      commonjs2: 'fs'
+    },
+    'process': {
+      commonjs: 'process',
+      commonjs2: 'process'
+    },
+    'util': {
+      commonjs: 'util',
+      commonjs2: 'util'
+    }
   },
   plugins: [
-    new CleanWebpackPlugin([PATHS.dist]),
-    ExtractCSSPlugin,
-    new UglifyJsPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: `${APP_PATHS.html}/index.html`,
-      minify: {
-        collapseBooleanAttributes: true,
-        collapseInlineTagWhitespace: true,
-        collapseWhitespace: true,
-        conservativeCollapse: true,
-        html5: true,
-        minifyCSS: true,
-        minifyJS: true,
-        removeAttributeQuotes: true,
-        removeComments: true,
-        removeOptionalTags: true
-      },
-      inlineSource: '.(js|css)$',
-      title: 'Andrew Hunt | Software Developer'
+    new CleanWebpackPlugin([PATHS.dist], {
+      allowExternal: true
     }),
-    new HtmlWebpackInlineSourcePlugin()
+    new UglifyJsPlugin()
   ],
   module: {
     rules: [{
-      test: /\.scss$/,
-      use: ExtractCSSPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            minimize: true
-          }
-        }, {
-          loader: 'sass-loader'
-        }]
-      })
-    }, {
       test: /\.js?$/,
+      exclude: /node_modules/,
       use: [{
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env'],
-          plugins: []
+          presets: [
+            ['@babel/preset-env', {
+              'targets': {
+                'node': true
+              }
+            }]
+          ],
+          plugins: [
+            require('@babel/plugin-transform-async-to-generator')
+          ]
         }
       }, {
-        loader: 'eslint-loader'
+        loader: 'eslint-loader',
+        options: {
+          emitError: false
+        }
       }]
     }]
   }
